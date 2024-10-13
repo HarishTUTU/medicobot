9993510b5527702933e82208a1e4712d0e508d4e
HarishTUTU <231801050@rajalakshmi.edu.in>
2024-10-13 11:10:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'HarishTUTU',
+    author_email= '231801050@rajalakshmi.edu.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
